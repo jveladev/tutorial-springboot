a1a1ac9ab13d20e518b5f3bbddcde1b80c94909f
Jorge <jvela@isclabs.com>
2023-03-22 19:21:26 +0100

    Segunda parte Spring Security

--- src/main/java/com/oesia/aytozgz/curso/srpingboot/config/SecurityConfiguration.java
+++ src/main/java/com/oesia/aytozgz/curso/srpingboot/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.oesia.aytozgz.curso.srpingboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
     @Bean
@@ -15,8 +22,8 @@ public class SecurityConfiguration {
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/img/**").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                //.antMatchers("/productos/**").hasAnyRole("USER")
-                .antMatchers("/productos/**").fullyAuthenticated()
+                .antMatchers("/productos/**").hasAnyRole("USER", "ADMIN")
+                //.antMatchers("/productos/**").fullyAuthenticated()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -28,6 +35,23 @@ public class SecurityConfiguration {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login");
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jvela")
+                .password(passwordEncoder.encode("jvela"))
+                .roles("USER")
+                .and()
+                .withUser("paula")
+                .password(passwordEncoder.encode("paula"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN");
     }
 
 }
--- src/main/java/com/oesia/aytozgz/curso/srpingboot/controller/ProductoController.java
+++ src/main/java/com/oesia/aytozgz/curso/srpingboot/controller/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -53,6 +54,7 @@ public class ProductoController {
 	}
 	
 	@GetMapping("/eliminar/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String eliminar(@PathVariable(value="id") Integer idProducto) {
 		productoService.eliminar(idProducto);
 		return "redirect:../" + VISTA_LISTA;
