9f494990b7dd24063a82115adb77a8a4558d309d
Jorge <jvela@isclabs.com>
2023-03-22 17:16:27 +0100

    Añado título al login

--- src/main/java/com/oesia/aytozgz/curso/srpingboot/controller/LoginController.java
+++ src/main/java/com/oesia/aytozgz/curso/srpingboot/controller/LoginController.java
@@ -1,17 +1,21 @@
 package com.oesia.aytozgz.curso.srpingboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping()
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(Map<String, Object> model, @Value("${aplicacion.nombre}") final String nombreAplicacion) {
+        model.put("titulo", nombreAplicacion);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication instanceof AnonymousAuthenticationToken) {
             return "login";
